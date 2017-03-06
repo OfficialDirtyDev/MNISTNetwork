600c818341637fbd3fc89bc5890cf3bbad9d1149
JanBH <jan.b.h.wilhelm@gmx.de>
2017-03-06 20:21:25 +0100

    Changed a few fields (added the "private" modifier)

--- MNISTNetwork/src/gui/MainGui.java
+++ MNISTNetwork/src/gui/MainGui.java
@@ -73,8 +73,8 @@ public class MainGui extends JFrame {
 	private JLabel info;
 	private static MainGui instance;
 	private final Network network;
-	Backpropagator back;
-	TrainingDataGenerator trainingDataGenerator;
+	private Backpropagator back;
+	private TrainingDataGenerator trainingDataGenerator;
 	private ApplicationFrame chartFrame;
 
 	public static void main(String[] args) {
